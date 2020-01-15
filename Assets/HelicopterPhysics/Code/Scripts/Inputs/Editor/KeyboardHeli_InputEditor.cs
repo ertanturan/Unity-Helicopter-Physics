94cc57da28d1539c7c592dd73609443faf08dcf9
Ertan Turan <ertanturan@outlook.com>
2020-01-15 17:23:53 +0300

    Keyboard input editor script developed.
    Now we can observe inputs without exposing them from scripts.

---
+++ Assets/HelicopterPhysics/Code/Scripts/Inputs/Editor/KeyboardHeli_InputEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+
+
+namespace HelicopterPhysics.Inputs.Editorial
+{
+    [CustomEditor(typeof(KeyboardHeliInput))]
+    public class KeyboardHeli_InputEditor : Editor
+    {
+        private KeyboardHeliInput _target;
+
+        private void OnEnable()
+        {
+            _target = (KeyboardHeliInput)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawDebugLayout();
+            Repaint();
+        }
+
+        private void DrawDebugLayout()
+        {
+
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Inputs", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            EditorGUI.indentLevel++;
+            EditorGUILayout.LabelField("Throttle : " + _target.ThrottleInput.ToString("0.00"));
+            EditorGUILayout.LabelField("Collective : " + _target.CollectiveInput.ToString("0.00"));
+            EditorGUILayout.LabelField("Cyclic : " + _target.CyclicInput.ToString("0.00"));
+            EditorGUILayout.LabelField("Pedal : " + _target.PedalInput.ToString("0.00"));
+
+            EditorGUILayout.EndVertical();
+        }
+    }
+}
