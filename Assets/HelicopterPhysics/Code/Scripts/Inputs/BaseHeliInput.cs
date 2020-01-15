3dfc89bb890dae398fea4f9be41967edd5e31e5b
Ertan Turan <ertanturan@outlook.com>
2020-01-15 16:59:12 +0300

    Base input system set.

---
+++ Assets/HelicopterPhysics/Code/Scripts/Inputs/BaseHeliInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace HelicopterPhysics.Inputs
+{
+    public class BaseHeliInput : MonoBehaviour
+    {
+
+        public float Vertical
+        {
+            get { return _Vertical; }
+        }
+        public float Horizontal
+        {
+            get { return _Horizontal; }
+        }
+
+
+        private float _Vertical = 0f;
+        private float _Horizontal = 0f;
+
+
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        protected virtual void HandleInput()
+        {
+            _Vertical = Input.GetAxis("Vertical");
+            _Horizontal = Input.GetAxis("Horizontal");
+        }
+    }
+}
+
---
+++ Assets/HelicopterPhysics/Code/Scripts/Inputs/KeyboardHeliInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+namespace HelicopterPhysics.Inputs
+{
+    public class KeyboardHeliInput : BaseHeliInput
+    {
+
+        public float ThrottleInput { get { return _throttleInput; } }
+        public float CollectiveInput { get { return _collectiveInput; } }
+        public Vector2 CyclicInput { get { return _cyclicInput; } }
+        public float PedalInput { get { return _pedalInput; } }
+
+
+        private float _throttleInput = 0f;
+        private float _collectiveInput = 0f;
+        private Vector2 _cyclicInput = Vector2.zero;
+        private float _pedalInput = 0f;
+
+        protected override void HandleInput()
+        {
+            base.HandleInput();
+
+            HandleThrottle();
+            HandleCollective();
+            HandleCyclic();
+            HandlePedal();
+
+        }
+
+
+        private void HandleThrottle()
+        {
+            _throttleInput = Vertical;
+        }
+
+        private void HandlePedal()
+        {
+            _pedalInput = Horizontal;
+        }
+
+        private void HandleCollective()
+        {
+
+        }
+
+
+        private void HandleCyclic()
+        {
+            _cyclicInput.y = Vertical;
+            _cyclicInput.x = Horizontal;
+        }
+
+    }
+
+}
