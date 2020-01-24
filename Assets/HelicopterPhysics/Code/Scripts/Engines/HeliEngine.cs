b5eb06014f4a5e7deb9a81657a61147f9ec28609
Ertan Turan <ertanturan@outlook.com>
2020-01-24 17:01:49 +0300

    base engine set

---
+++ Assets/HelicopterPhysics/Code/Scripts/Engines/HeliEngine.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace HelicopterPhysics.Physics
+{
+    public class HeliEngine : MonoBehaviour
+    {
+        public float MaxHP = 140f;
+        public float MaxRPM = 2700f;
+        public float PowerDelay = 2f;
+
+
+        private float _currentHp;
+        public float CurrentHP
+        {
+            get { return _currentHp; }
+        }
+
+        private float _currenRPM;
+        public float CurrentRPM
+        {
+            get { return _currenRPM; }
+        }
+
+        private void Start()
+        {
+
+        }
+
+        public void UpdateEngine(float throttleInput)
+        {
+
+
+
+        }
+
+    }
+}
+
