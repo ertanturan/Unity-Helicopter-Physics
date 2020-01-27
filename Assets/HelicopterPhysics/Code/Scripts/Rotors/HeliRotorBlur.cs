3d5f75aeebaf4ad5deb5fa8a3ae30c618b96f9e9
Ertan Turan <ertanturan@outlook.com>
2020-01-27 15:11:16 +0300

    Update HeliRotorBlur.cs

--- Assets/HelicopterPhysics/Code/Scripts/Rotors/HeliRotorBlur.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Rotors/HeliRotorBlur.cs
@@ -1,4 +1,5 @@
 ﻿using HelicopterPhysics.Inputs;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace HelicopterPhysics.Mechanics.Rotors
@@ -12,10 +13,20 @@ namespace HelicopterPhysics.Mechanics.Rotors
         public GameObject RightRotor;
         public GameObject BlurGeo;
 
+        public float MaxDPS = 1000f;
+
+        public List<GameObject> Blades = new List<GameObject>();
+        public List<Texture2D> BlurTextures = new List<Texture2D>();
 
         public void UpdateRotor(float dps, InputController inputController)
         {
             Debug.Log("Rotor blur .");
+
+            float normalizedDPS = Mathf.InverseLerp(0f, MaxDPS, dps);
+            Debug.Log(normalizedDPS);
+            int blurTexID = Mathf.FloorToInt(normalizedDPS * BlurTextures.Count);
+            blurTexID = Mathf.Clamp(blurTexID, 0, BlurTextures.Count - 1);
+
         }
 
     }
