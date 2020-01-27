979518ea6fa92509bd3479184dda5681fe3ad5ac
Ertan Turan <ertanturan@outlook.com>
2020-01-27 15:16:19 +0300

    blur textures lod system set.

--- Assets/HelicopterPhysics/Code/Scripts/Rotors/HeliRotorBlur.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Rotors/HeliRotorBlur.cs
@@ -15,17 +15,22 @@ namespace HelicopterPhysics.Mechanics.Rotors
 
         public float MaxDPS = 1000f;
 
+        public Material BlurMat;
+        [Space]
         public List<GameObject> Blades = new List<GameObject>();
         public List<Texture2D> BlurTextures = new List<Texture2D>();
 
         public void UpdateRotor(float dps, InputController inputController)
         {
-            Debug.Log("Rotor blur .");
 
             float normalizedDPS = Mathf.InverseLerp(0f, MaxDPS, dps);
-            Debug.Log(normalizedDPS);
             int blurTexID = Mathf.FloorToInt(normalizedDPS * BlurTextures.Count);
             blurTexID = Mathf.Clamp(blurTexID, 0, BlurTextures.Count - 1);
+            Debug.Log(blurTexID);
+            if (BlurMat && BlurTextures.Count > 0)
+            {
+                BlurMat.SetTexture("_MainTex", BlurTextures[blurTexID]);
+            }
 
         }
 
