9952b2cfbdbe208a628b463a1d329a706fa65b8f
Ertan Turan <ertanturan@outlook.com>
2020-01-29 10:38:54 +0300

    basic follow camera set for helicopter

--- Assets/HelicopterPhysics/Code/Scripts/Cameras/HeliFollowCamera.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Cameras/HeliFollowCamera.cs
@@ -10,7 +10,11 @@ namespace HelicopterPhysics.Cameras
         public Rigidbody Rb;
         public float Height = 2f;
         public float Distance = 2f;
+        public float CameraSpeed = 2f;
+        private Vector3 _currentVelocity = Vector3.zero;
 
+        public Transform LookatTarget;
+        private Vector3 _targetPos;
 
         public void FixedUpdate()
         {
@@ -22,7 +26,19 @@ namespace HelicopterPhysics.Cameras
 
         public void HandleCamera()
         {
-            Debug.Log("Handling camera ..");
+            //Debug.Log("Handling camera ..");
+
+            Vector3 flatforward = -Rb.transform.forward;
+            flatforward.y = 0f;
+            flatforward = flatforward.normalized;
+
+            _targetPos = Rb.position + (flatforward * Distance) + (Vector3.up * Height);
+
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPos,
+                ref _currentVelocity, CameraSpeed);
+
+            transform.LookAt(LookatTarget);
+
         }
     }
 }
