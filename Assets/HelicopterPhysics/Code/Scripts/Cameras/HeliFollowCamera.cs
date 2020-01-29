a25cd1492de4e276b40011add6a91af3102e0db1
Ertan Turan <ertanturan@outlook.com>
2020-01-29 10:19:10 +0300

    camera handling system set.

--- Assets/HelicopterPhysics/Code/Scripts/Cameras/HeliFollowCamera.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Cameras/HeliFollowCamera.cs
@@ -5,9 +5,24 @@ namespace HelicopterPhysics.Cameras
 
     public class HeliFollowCamera : MonoBehaviour, IHeliCamera
     {
+
+        [Header("Camera Properties")]
+        public Rigidbody Rb;
+        public float Height = 2f;
+        public float Distance = 2f;
+
+
+        public void FixedUpdate()
+        {
+            if (Rb)
+            {
+                HandleCamera();
+            }
+        }
+
         public void HandleCamera()
         {
-
+            Debug.Log("Handling camera ..");
         }
     }
 }
