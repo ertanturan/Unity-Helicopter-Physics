b10fce9c00ebe1e22ddbaeeee43f13b53e272201
Ertan Turan <ertanturan@outlook.com>
2020-02-09 17:12:18 +0300

    applying changes to the scene

--- Assets/HelicopterPhysics/Code/Scripts/Cameras/AdvancedHeliCamera.cs
+++ Assets/HelicopterPhysics/Code/Scripts/Cameras/AdvancedHeliCamera.cs
@@ -1,8 +1,12 @@
-﻿namespace HelicopterPhysics.Cameras
+﻿using UnityEngine;
+
+namespace HelicopterPhysics.Cameras
 {
     public class AdvancedHeliCamera : BaseHeliCamera
     {
-
+        [Header("Advanced Camera Properties")]
+        public float MinDistance = 4f;
+        public float MaxDistance = 8f;
 
     }
 
