700f1a9b75ccef31dfbf7759457069ad85441c5d
Ertan Turan <ertanturan@outlook.com>
2020-04-17 19:14:12 +0300

    showing gauge on UI

---
+++ Assets/HelicopterPhysics/Code/Scripts/UI/GaguePanel.cs
@@ -0,0 +1,30 @@
+﻿using HelicopterPhysics.Inputs;
+using TMPro;
+using UnityEngine;
+
+
+public class GaguePanel : MonoBehaviour
+{
+    [Header("UI Properties")]
+    public TextMeshProUGUI RawThrottle;
+    public TextMeshProUGUI StickyThrottle;
+    public TextMeshProUGUI Collective;
+    public TextMeshProUGUI StickyCollective;
+    public TextMeshProUGUI Cyclic;
+    public TextMeshProUGUI Pedal;
+
+
+    [Header("Other References")]
+    public KeyboardHeliInput Input;
+
+    private void Update()
+    {
+        RawThrottle.text = Input.ThrottleInput.ToString();
+        StickyThrottle.text = Input.StickyThrottle.ToString();
+        Collective.text = Input.CollectiveInput.ToString();
+        StickyCollective.text = Input.StickyCollective.ToString();
+        Cyclic.text = "(" + Input.CyclicInput.x + "," + Input.CyclicInput.y + ")";
+        Pedal.text = Input.PedalInput.ToString();
+    }
+
+}
